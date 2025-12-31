22ed825ca4658b10f3e95f805a10e880a3b60b1d
nhone12 <nhonea12@gmail.com>
2025-12-30 23:15:46 -0500

    Update time of github action to pull shot data at 6:00 AM ET instead of 3:00 AM ET. Make app always read in the rds file of shot data

--- app.R
+++ app.R
@@ -14,41 +14,8 @@ font_add_google("Roboto", "roboto")
 # Enable showtext for plots
 showtext_auto()
 
-if (!file.exists("wbb_shots.rds")) {
-# load NCAA women's basketball play-by-play data
-wbb_pbp <- wehoop::load_wbb_pbp()
-
-# load player box score data
-player_box_scores <- load_wbb_player_box()
-
-# add player names and headshots to play-by-play data
-wbb_pbp <- wbb_pbp |> 
-  left_join(player_box_scores, by = c("athlete_id_1" = "athlete_id", 
-                                      "team_id" = "team_id",
-                                      "season" = "season",
-                                      "season_type" = "season_type",
-                                      "game_id" = "game_id",
-                                      "game_date" = "game_date",
-                                      "game_date_time" = "game_date_time"))
-
-
-# get just shoooting plays from the play-by-play data and adjust their coordinates
-wbb_shots <- wbb_pbp |> 
-  filter(
-    shooting_play == TRUE, 
-    !(type_text %in% c("MadeFreeThrow", "MissedFreeThrow"))
-  ) |> 
-  mutate(
-    loc_x = -1*(coordinate_x_raw - 25),
-    loc_y = coordinate_y_raw + 5
-  )
-
-saveRDS(wbb_shots, "wbb_shots.rds")
-} else {
-  wbb_shots <- readRDS("wbb_shots.rds")
-}
-
-
+# read in shot data from rds file (rds file created in shot_data.R)
+wbb_shots <- readRDS("wbb_shots.rds")
 
 # create the court points
 ##Draw court (from https://raw.githubusercontent.com/Henryjean/NBA-Court/refs/heads/main/CourtDimensions.R) (dimensions changed from NBA to college)
